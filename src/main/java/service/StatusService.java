c734e0fabb9cd9f33fdf9caff9c011097223407e
ducutc94 <ducutc94@gmail.com>
2023-06-03 10:52:37 +0700

    service

--- src/main/java/service/StatusService.java
+++ src/main/java/service/StatusService.java
@@ -1,4 +1,51 @@
 package service;
 
-public class StatusService {
+import DAO.StatusDAO;
+import model.Status;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatusService implements Service<Status> {
+    private final List<Status> statusList;
+    private static StatusService statusService;
+    private final StatusDAO statusDAO;
+
+    private StatusService() {
+        statusList = new ArrayList<>();
+        statusDAO=new StatusDAO();
+    }
+
+    public static StatusService getInstance() {
+        if (statusService == null) {
+            statusService = new StatusService();
+        }
+        return statusService;
+    }
+
+    @Override
+    public List<Status> getList() {
+        return null;
+    }
+
+    @Override
+    public void create(Status status) {
+
+    }
+
+    @Override
+    public void update(Status status) {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void displayALL() {
+
+    }
 }
